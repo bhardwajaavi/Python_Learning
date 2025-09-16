9fdb7737cf50f0ee8fc71b9526182c13b89599a3
bhardwajaavi <bhardwajaavi@gmail.com>
2025-09-16 20:04:13 +0530

    updated

--- Python Dictionaries/remove.py
+++ Python Dictionaries/remove.py
@@ -8,3 +8,14 @@ thisdict = {
 }
 thisdict.pop("model")
 print(thisdict)
+
+
+#The clear() method empties the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
