4408e6d07ab367b6811d65ca7d1517e5c3008019
bhardwajaavi <bhardwajaavi@gmail.com>
2025-10-12 15:26:05 +0530

    initial commit

---
+++ Python Classes:Objects/create class.py
@@ -0,0 +1,17 @@
+#Python Classes and Objects
+
+'''Python Classes/Objects
+Python is an object oriented programming language.
+Almost everything in Python is an object, with its properties and methods.
+A Class is like an object constructor, or a "blueprint" for creating objects.
+
+'''
+
+
+#Create a Class
+'''To create a class, use the keyword class:'''
+
+class Family:
+  x = 7
+
+print(Family)
---
+++ Python Classes:Objects/create object.py
@@ -0,0 +1,10 @@
+#Create Object
+'''Now we can use the class named MyClass to create objects:
+
+Example
+Create an object named p1, and print the value of x:'''
+class Family:
+  x= 7
+
+p1 = Family()
+print(p1.x)
