f97acc59a32b9a9cc647b0ae0d0d81907bb8d2bd
bhardwajaavi <bhardwajaavi@gmail.com>
2025-10-26 23:49:18 +0530

    initial commit

--- Python Inheritance/creating child class.py
+++ Python Inheritance/creating child class.py
@@ -21,3 +21,32 @@ class Student(Person):
 
 x = Student("aavi", "Bhardwaj")
 x.printname()
+
+
+
+
+
+
+'''Add the __init__() Function
+So far we have created a child class that inherits the properties and methods from its parent.
+
+We want to add the __init__() function to the child class (instead of the pass keyword).
+
+Note: The __init__() function is called automatically every time the class is being used to create a new object.
+
+'''
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+x = Student("Aavi", "Bhardwaj")
+x.printname()
