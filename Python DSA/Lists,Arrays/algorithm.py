d5d07f15ccebef976e68749bc72f6dce2d03cf98
bhardwajaavi <bhardwajaavi@gmail.com>
2025-11-11 19:09:25 +0530

    initial commit

---
+++ Python DSA/Lists,Arrays/algorithm.py
@@ -0,0 +1,16 @@
+'''Sometimes we want to perform actions that are not built into Python.
+
+Then we can create our own algorithms.
+
+For example, an algorithm can be used to find the lowest value in a list, like in the example below:
+
+'''
+
+#Create an algorithm to find the lowest value in a list:
+
+x=[5,6,4,8,2,9,7,0]
+min_value=x[0]
+for i in x:
+    if i < min_value:
+        min_value=i
+print("lowest Value is:",min_value)
---
+++ Python DSA/Lists,Arrays/lists methods.py
@@ -0,0 +1,9 @@
+#python lists come with several built in algorithm which also called as methods. to perform common operations like appending, sorting and more.
+
+x=[1,2,3,4,5,6,7,9]
+x.append(5)
+print(x)
+
+
+x.sort()
+print(x)
