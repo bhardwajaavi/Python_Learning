577328e13a81d30491b40464811bd08488d33df2
bhardwajaavi <bhardwajaavi@gmail.com>
2025-11-13 05:46:59 +0530

    initial commit

---
+++ Python DSA/Queues/queue.py
@@ -0,0 +1,11 @@
+#we will conquere covi-19
+
+class stack:
+    def __init__(self):
+        self.stack()
+    def push(self,element):
+        self.stack.append(element)
+        
+mystack=stack()
+mystack.push("we will conquere covi-19")
+print("the stack is:",mystack.stack)
