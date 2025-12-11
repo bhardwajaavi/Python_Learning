c02247cff4ce55c430b51c27ca2c0574f04b48bd
bhardwajaavi <bhardwajaavi@gmail.com>
2025-12-12 02:16:12 +0530

    initial commit

--- Python DSA/Lists,Arrays/list.py
+++ Python DSA/Lists,Arrays/list.py
@@ -14,4 +14,10 @@ z=[1,"aavi",3.14,4,"Bhardwaj"]      #list with mixed values
 print(x)
 print(y)
 print(z)
-     
+
+
+'''List is a built in data structure used to store multiple data elements.
+Lists are used by Many Algorithms'''
+
+#Creating a Lists
+'''Lists are created by Square Bracket []'''
--- Python Lists/list.py
+++ Python Lists/list.py
@@ -54,3 +54,11 @@ List is a collection which is ordered and changeable. Allows duplicate members.
 Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
 Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
 Dictionary is a collection which is ordered** and changeable. No duplicate members."""
+
+#List
+'''Lists are used to store multiple items in a single variable.
+Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+Lists are created using square brackets:'''
+
+names = ["Aavi", "Satish", "Anita", "Aneesh"]
+print(names)
